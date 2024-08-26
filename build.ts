780852654e684ddcf74585b57a9035a5c3514259
mazrean <shunsuke.w04@gmail.com>
2024-08-27 04:48:38 +0900

    fix bundle

--- build.ts
+++ build.ts
@@ -1,14 +1,16 @@
-import esbuild from "esbuild";
+import esbuild from 'esbuild'
 
 await esbuild.build({
   bundle: true,
-  entryPoints: ["./src/main.ts"],
-  outdir: "./dist",
+  entryPoints: ['./src/main.ts'],
+  outdir: './dist',
   outExtension: {
     // 必須では無いが、ESM形式で出力されることを明示的にするため拡張子を.mjsにしている
-    ".js": ".mjs",
+    '.js': '.mjs'
   },
-  platform: "node",
-  format: "esm",
-  packages: "external",
-});
+  platform: 'node',
+  format: 'esm',
+  banner: {
+    js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));'
+  }
+})
